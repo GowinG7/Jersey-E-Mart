fa5e6d9a0e9b2e5325d5c34e96ae0ea71aba49db
Gobinda Ghimire <gowingg1@gmail.com>
2025-11-19 09:33:26 +0545

    adding css animate cnd links

--- shared/commonlinks.php
+++ shared/commonlinks.php
@@ -3,7 +3,10 @@
 
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" />
 
- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"></script>
 
- <!-- bootstrap Icons CDN -->
+<!-- bootstrap Icons CDN -->
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css">
+
+<!-- animate css cdn links (yo maile home page ko jersey page ma main text ma used grya xu) -->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" />
